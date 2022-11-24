b33af827583cc0ca105bd0cd97fd54102178d130
marcocarneiro <81334230+brebueno@users.noreply.github.com>
2022-11-24 12:52:36 -0200

    Instalacao_laravel

--- routes/web.php
+++ routes/web.php
@@ -49,3 +49,13 @@ Route::get('/login', function () {
     return view('login');
 });
 
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
